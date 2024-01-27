6d9ba167ff3dca042f81160d1a40241b65486c40
hasibprangon <hasibulhossainp09@gmail.com>
2024-01-27 21:14:13 +0600

    Start creating Nav section

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <nav>
+            <div className='container mx-auto flex items-center justify-between'>
+                <h1 className=' text-3xl  font-bold'>Job-Hunter</h1>
+                <div className='flex gap-4'>
+                    <Link>Statistics</Link>
+                    <Link>Applied Jobs</Link>
+                    <Link>Blog</Link>
+                </div>
+                <button className=' border rounded-lg px-5 py-3 bg-blue-500 hover:bg-blue-700 text-white'>Start Apply</button>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
