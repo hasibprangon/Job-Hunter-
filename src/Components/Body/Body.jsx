1ffa9e8b58ff3eef0fa5f27b57e95d759774041e
hasibprangon <hasibulhossainp09@gmail.com>
2024-01-28 11:58:40 +0600

    header of body section done

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import User from '../../../public/user.png'
+import './Body.css'
 const Body = () => {
     return (
-        <div className=' container flex items-center justify-between mx-auto'>
+        <div className='body-section container flex items-center justify-between mx-auto'>
             <div>
-                <h1 className=' text-6xl font-semibold'>One Step Closer To Your <span className=' text-indigo-500'> Dream Job</span></h1>
-                <p><small>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</small></p>
-                <button>Get Started</button>
+                <h1 className=' text-6xl font-semibold text-left'>One Step Closer To Your <span className=' text-indigo-500'> Dream Job</span></h1>
+                <p className=' text-left mt-3'><small>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</small></p>
+                <div className=' flex items-start mt-3 '>
+                <button className=' bg-indigo-500 rounded-md hover:bg-indigo-700 px-5 py-3 text-white'>Get Started</button>
+                </div>
             </div>
-            <img className=' h-96 mt-10' src={User} alt="" />
+            <img className='userImg mt-10' src={User} alt="" />
         </div>
     );
 };
