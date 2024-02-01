c7ebcd0313dbf4df442b7d8045b49076e9173dd8
hasibprangon <hasibulhossainp09@gmail.com>
2024-02-01 19:22:44 +0600

    Showing all assignment marks

--- src/Components/AppliedJobs/AppliedDetails.jsx
+++ src/Components/AppliedJobs/AppliedDetails.jsx
@@ -5,25 +5,27 @@ import { Link } from 'react-router-dom';
 const AppliedDetails = ({ data }) => {
     const { id, companyLogo, jobTitle, companyName, remoteOrOnsite, location, fulltimeOrPartTime, salary } = data;
     return (
-        <div className=' border bg-indigo-200 p-5 text-left rounded-md flex flex-col h-full'>
-            <div className='flex-grow'>
-                <img className=' w-44' src={companyLogo} alt="" />
+        <div className=' border p-5 text-left rounded-md grid grid-cols-3'>
+            <div className='flex'>
+                <img className=' w-36' src={companyLogo} alt="" />
 
-                <h3 className=' text-2xl font-bold mt-4'>{jobTitle}</h3>
+                <div className='ml-10'>
+                    <h3 className=' text-2xl font-bold mt-4'>{jobTitle}</h3>
 
-                <p className=' font-semibold text-2xl mb-3'><small>{companyName}</small></p>
+                    <p className=' font-semibold text-2xl mb-3'><small>{companyName}</small></p>
 
-                <span className='border rounded-md border-indigo-400 font-semibold text-indigo-400 px-2 py-1'>{remoteOrOnsite}</span>
+                    <span className='border rounded-md border-indigo-400 font-semibold text-indigo-400 px-2 py-1'>{remoteOrOnsite}</span>
 
-                <span className='border rounded-md border-indigo-400 font-semibold text-indigo-400 px-2 py-1 mx-1'>{fulltimeOrPartTime}</span>
+                    <span className='border rounded-md border-indigo-400 font-semibold text-indigo-400 px-2 py-1 mx-1'>{fulltimeOrPartTime}</span>
 
-                <p className='mt-3 font-semibold flex items-center'><MapPinIcon className="h-4 w-4" /><span className='mr-2'>{location}</span>
+                    <p className='mt-3 font-semibold flex items-center'><MapPinIcon className="h-4 w-4" /><span className='mr-2'>{location}</span>
 
-                    <span className='flex items-center mr-3'> <CurrencyDollarIcon className='h-4 w-4'></CurrencyDollarIcon> Salary:{salary}</span>
-                </p>
+                        <span className='flex items-center mr-3'> <CurrencyDollarIcon className='h-4 w-4'></CurrencyDollarIcon> Salary:{salary}</span>
+                    </p>
+                </div>
             </div>
 
-            <div className="mt-auto">
+            <div className='mx-auto my-auto'>
                 <Link to={`/jobDetails/${id}`}>
                     <button className=' bg-indigo-500 rounded-md hover:bg-indigo-700 px-5 py-3 text-white'>View Details
                     </button>
--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,9 +1,60 @@
 import React from 'react';
+import {
+    ComposedChart,
+    Area,
+    XAxis,
+    YAxis,
+    Tooltip,
+   
+  } from 'recharts';
 
 const Statistics = () => {
+    const assignmentData = [
+        {
+          name: "Assignment-1",
+          marks: 60,
+        },
+        {
+          name: "Assignment-2",
+          marks: 55,
+        },
+        {
+          name: "Assignment-3",
+          marks: 60,
+        },
+        {
+          name: "Assignment-4",
+          marks: 57,
+        },
+        {
+          name: "Assignment-5",
+          marks: 27,
+        },
+        {
+            name: "Assignment-6",
+            marks: 28,
+          },
+          {
+            name: "Assignment-7",
+            marks: 20,
+          },
+          {
+            name: "Assignment-8",
+            marks: 30,
+          },
+      ];
     return (
         <div>
-            <h1>this is statistics page</h1>
+            <h1>Assignments mark 1-8</h1>
+            <div>
+                <ComposedChart width={1200} height={500} data={assignmentData}>
+                <Area dataKey="marks"></Area>
+                <XAxis dataKey="name" ></XAxis>
+                <YAxis></YAxis>
+                <Tooltip></Tooltip>
+                </ComposedChart>
+                
+            </div>
         </div>
     );
 };
