7942d871904ba4120edf9817e95f76a6391f0ec9
hasibprangon <hasibulhossainp09@gmail.com>
2024-02-01 20:23:42 +0600

    Showing Active link

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ActiveLink from '../ActiveLink/ActiveLink';
 
 const Header = () => {
     return (
@@ -7,10 +8,10 @@ const Header = () => {
             <div className='container mx-auto flex items-center justify-between'>
                 <h1 className=' text-3xl  font-bold'>Job-Hunter</h1>
                 <div className='flex gap-4'>
-                    <Link to='/'>Home</Link>
-                    <Link to='/statistics'>Statistics</Link>
-                    <Link to='/appliedJobs'>Applied Jobs</Link>
-                    <Link to='/blog'>Blog</Link>
+                    <ActiveLink to='/'>Home</ActiveLink>
+                    <ActiveLink to='/statistics'>Statistics</ActiveLink>
+                    <ActiveLink to='/appliedJobs'>Applied Jobs</ActiveLink>
+                    <ActiveLink to='/blog'>Blog</ActiveLink>
                 </div>
                 <button className=' border rounded-lg px-5 py-3 bg-blue-500 hover:bg-blue-700 text-white'>Start Apply</button>
             </div>
